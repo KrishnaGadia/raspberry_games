7cad25284e1dbff95ad8d0149bd225e134721565
Krishna Gadia <krishnagadia3@gmail.com>
2020-12-05 01:24:04 +0530

    Update controller.py
    adding new games

--- controller.py
+++ controller.py
@@ -11,24 +11,28 @@ begin = False
 def display():
     global selection
     if(selection == 1):
-        return "[*]car []frog []snake"
+        return "[*]car []frog []pong []snake []TicTacToe"
     if(selection == 2):
-        return "[]car [*]frog []snake"
+        return "[]car [*]frog []pong []snake []TicTacToe"
     if(selection = 3):
-        return "[]car []frog [*]snake"
+        return "[]car []frog [*]pong []snake []TicTacToe"
+    if(selection == 4):
+        return "[]car []frog []pong [*]snake []TicTacToe"
+    if(selection = 5):
+        return "[]car []frog []pong []snake [*]TicTacToe"
 
 def start():
     global begin
     begin = True
     
-def left():
+def left(event):
     global selection
-    if(selection<3):
+    if event.action=='pressed' and selection<5:
         selection+=1
         
 def right():
     global selection
-    if(selection>1):
+    if event.action=='pressed' and selection>1:
         selection-=1
 
 sense.stick.direction_up = left
@@ -38,8 +42,12 @@ sense.stick.middle = start
 while not begin:
     sense.show_message(display())
 if(selection == 1):
-    import car
+    import car_race
 if(selection == 2):
     import frog 
 if(selection = 3):
+    import pong
+if(selection == 4):
     import snake
+if(selection = 5):
+    import TicTacToe
